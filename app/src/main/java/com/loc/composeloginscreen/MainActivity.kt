c924e42f9a4c1a9bc69984a6fce4e137c6fbcb29
Mohammad nawas <mohammednawas8@gmail.com>
2023-06-17 00:52:35 +0300

    Init

---
+++ app/src/main/java/com/loc/composeloginscreen/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.loc.composeloginscreen
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.loc.composeloginscreen.ui.theme.ComposeLoginScreenTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeLoginScreenTheme {
+
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/loc/composeloginscreen/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.loc.composeloginscreen.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Black = Color(0xFF000113)
+val LightBlueWhite = Color(0xFFF1F5F9) //Social media background
+val BlueGray = Color(0xFF334155)
