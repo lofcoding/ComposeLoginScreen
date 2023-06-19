e3006358373e32da9b24856c3f80e6b50367ce81
Mohammad nawas <mohammednawas8@gmail.com>
2023-06-19 09:25:40 +0300

    design

---
+++ app/src/main/java/com/loc/composeloginscreen/LoginTextField.kt
@@ -0,0 +1,55 @@
+package com.loc.composeloginscreen
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import com.loc.composeloginscreen.ui.theme.Black
+import com.loc.composeloginscreen.ui.theme.focusedTextFieldText
+import com.loc.composeloginscreen.ui.theme.textFieldContainer
+import com.loc.composeloginscreen.ui.theme.unfocusedTextFieldText
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LoginTextField(
+    modifier: Modifier = Modifier,
+    label: String,
+    trailing: String,
+) {
+
+    val uiColor = if (isSystemInDarkTheme()) Color.White else Black
+
+    TextField(
+        modifier = modifier,
+        value = "",
+        onValueChange = {},
+        label = {
+            Text(text = label, style = MaterialTheme.typography.labelMedium, color = uiColor)
+        },
+        colors = TextFieldDefaults.colors(
+            unfocusedPlaceholderColor = MaterialTheme.colorScheme.unfocusedTextFieldText,
+            focusedPlaceholderColor = MaterialTheme.colorScheme.focusedTextFieldText,
+            unfocusedContainerColor = MaterialTheme.colorScheme.textFieldContainer,
+            focusedContainerColor = MaterialTheme.colorScheme.textFieldContainer,
+        ),
+        trailingIcon = {
+            TextButton(onClick = { /*TODO*/ }) {
+                Text(
+                    text = trailing,
+                    style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.Medium),
+                    color = uiColor
+                )
+            }
+        }
+
+    )
+
+}
